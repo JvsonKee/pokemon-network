c8d5bad33f50fa10817dc0cec2e2026ebce884c2
JvsonKee <jasonk2723@gmail.com>
2025-03-01 21:11:39 -0700

    documentation and spacing

--- src/scraper/util.py
+++ src/scraper/util.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-
 # scrapes episode list from serebii.net
 # returns array of parsed episodes
 def scrape_episodes(driver):
