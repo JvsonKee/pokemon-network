5ead3fe2d72b30619436c8ccedfbf5f6dfa0d817
JvsonKee <jasonk2723@gmail.com>
2025-03-02 17:46:03 -0700

    data clean up

--- src/data_scripts/clean_pokemon.py
+++ src/data_scripts/clean_pokemon.py
@@ -2,24 +2,13 @@ import pandas as pd
 
 df = pd.read_csv(
     '../../datasets/raw/pokemon.csv', 
-    usecols=['id', 'name', 'rank', 'evolves_from', 'type1', 'type2']
+    usecols=['id', 'name', 'generation']
 )
 
 # remove last 119 pokemon paldea region
 df = df.iloc[:-120]
 
-# create dictionary mapping the name of the pokemon to their corresponding id
-name_to_id = df.set_index('name')['id'].to_dict()
-
-# replace the names in the "evolves_from" column with the corresponding id 
-df["evolves_from"] = df["evolves_from"].map(name_to_id)
-
-# replace missing values with -1 (pokemon that do not evolve from something)
-df["evolves_from"] = df["evolves_from"].fillna(-1)
-
-# covert to integers
-df["evolves_from"] = df["evolves_from"].astype(int)
-
+# renaming columns for gephi
 df = df.rename(columns={ 'id': 'Id', 'name': 'Label' })
 
 # export to csv file
