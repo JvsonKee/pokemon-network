92948f443789128ad7383c672f5bc4cc0164795a
JvsonKee <jasonk2723@gmail.com>
2025-03-02 20:40:21 -0700

    removed feature for adding 0 degree pokemon to edge list

--- src/scraper/util.py
+++ src/scraper/util.py
@@ -111,7 +111,6 @@ def scrape_pokemon(driver):
         for pokemon_index in range(1, 906):
             print(pokemon_index)
             id = get_id(pokemon_index)
-            count = 0
 
             driver.get(f"https://www.serebii.net/anime/dex/{id}.shtml")
 
@@ -150,10 +149,6 @@ def scrape_pokemon(driver):
                         continue
 
                     edges.add((pokemon_index, int(episode_id) + 10000))
-                    count += 1
-
-            if count == 0:
-                edges.add((pokemon_index, None))
 
         return edges
 
