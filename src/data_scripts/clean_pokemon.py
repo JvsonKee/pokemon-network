4c540ce60df8d772412dcaac5045c8f50a394a3a
JvsonKee <jasonk2723@gmail.com>
2025-04-04 13:39:19 -0600

    updated data

--- src/data_scripts/clean_pokemon.py
+++ src/data_scripts/clean_pokemon.py
@@ -1,21 +1,20 @@
 import pandas as pd
 
 df = pd.read_csv(
-    '../../datasets/raw/pokemon.csv', 
-    usecols=['id', 'name', 'generation']
+    "../../datasets/raw/pokemon.csv", usecols=["id", "name", "generation", "rank"]
 )
 
 # remove last 119 pokemon paldea region
 df = df.iloc[:-120]
 
 # create node_type column
-df['node_type'] = 'pokemon'
+df["node_type"] = "pokemon"
 
 # capatalize the first letter of the pokemon name
-df['name'] = df['name'].str.capitalize()
+df["name"] = df["name"].str.capitalize()
 
 # renaming columns for gephi
-df = df.rename(columns={ 'id': 'Id', 'name': 'Label' })
+df = df.rename(columns={"id": "Id", "name": "Label", "rank": "class"})
 
 # export to csv file
 df.to_csv("../../datasets/clean/pokemon_nodes.csv", index=False)
